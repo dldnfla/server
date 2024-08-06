e275918f0dd40bf07c04ebc3338a5969b1e01dcd
dldnfla <lwr0221716@naver.com>
2024-08-06 16:35:49 +0900

    success crud test

--- app/models/user.py
+++ app/models/user.py
@@ -7,12 +7,12 @@ from ..database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, nullable=False, index=True)
-    username = Column(String,unique_key= True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable= False, index=True)
+    username = Column(String,unique= True, nullable=False)
     fullname = Column(String)
     profile_image = Column(String)
     status_message = Column(String)
-    user_url = Column(String, nullable=False)
+    user_url = Column(String)
     music_info = Column(String)
     hashed_password = Column(String, nullable=False)
 
