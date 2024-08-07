168d353f9739cbfcf06b7d495cf7bb32382a40ed
yewon <128056568+jeffyea320@users.noreply.github.com>
2024-08-07 13:56:08 +0900

    Feedback Edit2

--- app/crud/dialog.py
+++ app/crud/dialog.py
@@ -15,12 +15,6 @@ def create_dialog(db: Session, dialog: schemas.DialogCreate):
 
     return db_dialog
 
-
-def get_dialog(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Dialog).slice(skip, limit).all()
-
-# 특정 유저의 방명록 목록만 가지고 와야되는 거라 user_id로 필터 걸어야됨
-
 def get_dialog(db: Session, user_id: int, skip: int = 0, limit: int = 100):
     return db.query(models.Dialog).filter(models.Dialog.user_id == user_id).offset(skip).limit(limit).all()
 
