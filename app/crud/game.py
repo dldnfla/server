dc0eacba96992edfa510b0d3064adecf029acaa7
dldnfla <lwr0221716@gmail.com>
2024-08-21 15:54:28 +0900

    add score crud

--- app/crud/game.py
+++ app/crud/game.py
@@ -4,20 +4,28 @@ from .. import models, oauth2, schemas
 
 
 def update_score(db: Session, score: schemas.ScoreCreate):
-    db.query(models.User).filter(models.User.username == score.username).update(
-        score.dict(exclude_unset=True)
-    )
-    db.commit()
+    db_score = get_score(db,username = score.username)
+
+    if db_score is None:
+        create_score(db,username=score.username,score=score.score)
 
     ...
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+def create_score(db: Session, score: schemas.ScoreCreate):
+    db_score = models.Score(
+        username = score.username,
+        score = score.score
+    )
+    db.add(db_score)
+    db.commit()
+    db.refresh(db_score)
+    return db_score
 
+    ...
 
-def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
+def get_score(db: Session, username: str):
+    return db.query(models.Score).filter(models.User.name== username).first()
 
 
 def update_user(db: Session, new_user: schemas.UserEdit, user: schemas.UserGet):
