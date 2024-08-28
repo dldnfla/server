2c908542448d84081598327b4f25d74a9fce43d0
dldnfla <lwr0221716@gmail.com>
2024-08-28 13:12:28 +0900

    ruff foramt

--- app/routers/upload.py
+++ app/routers/upload.py
@@ -1,6 +1,5 @@
 from typing import Annotated
 import boto3
-import logging
 import uuid
 from botocore.exceptions import ClientError
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
@@ -9,7 +8,7 @@ from sqlalchemy.orm import Session
 
 from app.database import get_db
 
-from .. import crud, models
+from .. import crud
 from app import oauth2, schemas
 
 
@@ -26,6 +25,34 @@ bucket = "ewootz-s3-bucket"
 
 
 @router.post("/upload", status_code=status.HTTP_200_OK)
+async def upload(
+    file: UploadFile,
+    current_user: Annotated[schemas.UserGet, Depends(oauth2.get_authenticated_user)],
+    db: Session = Depends(get_db),
+):
+    filename = f"{str(uuid.uuid4())}.jpg"
+    s3_key = f"/{filename}"
+
+    try:
+        client.upload_fileobj(file.file, bucket, s3_key)
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=f"S3 upload fails: {str(e)}")
+
+    url = "https://%s.s3.ap-northeast-2.amazonaws.com/%s" % (
+        bucket,
+        s3_key,
+    )
+
+    new_user = schemas.UserEdit(
+        profile_image=url,
+    )
+
+    crud.update_user(db, new_user, current_user)
+
+    return url
+
+
+@router.post("/board", status_code=status.HTTP_200_OK)
 async def upload(
     file: UploadFile,
     current_user: Annotated[schemas.UserGet, Depends(oauth2.get_authenticated_user)],
