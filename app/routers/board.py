7d918f7135d3a69ae1289db35094512a5655f95a
dldnfla <lwr0221716@gmail.com>
2024-09-05 20:26:34 +0900

    modify board get api

--- app/routers/board.py
+++ app/routers/board.py
@@ -28,9 +28,6 @@ def get_postlist(
     current_user: Annotated[schemas.UserAuth, Depends(oauth2.get_authenticated_user)],
     db: Session = Depends(get_db),
 ):
-    if current_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-
     return crud.get_all_posts(db)
 
 
@@ -43,14 +40,12 @@ def get_post(
     post_id: int,
     db: Session = Depends(get_db),
 ):
-    if current_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
 
     return crud.get_post(db, post_id=post_id)
 
 
 # 태그별 게시글 목록 가지고 오기
-@router.get("/tag/{tag}", status_code=status.HTTP_200_OK)
+@router.get("/tag/{category}", status_code=status.HTTP_200_OK)
 def get_post(
     current_user: Annotated[schemas.UserAuth, Depends(oauth2.get_authenticated_user)],
     category: str,
